b38b3d8f59d28de060a4ff619b1b8286209da99c
Akshaykeerti <akshaykeerti@gmail.com>
2025-09-01 11:39:00 +0400

    Final fix: Update ESLint config to treat unused vars as warnings for successful build

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -17,6 +17,7 @@ const eslintConfig = [
       'no-unused-vars': 'warn',
       'no-console': 'warn',
       'react/no-unescaped-entities': 'off',
+      '@typescript-eslint/no-unused-vars': 'warn',
     },
   },
 ];
